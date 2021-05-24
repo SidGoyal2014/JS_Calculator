3c86f76d6d113453f7726f9a402e4871fd82aa03
Siddharth Goyal <sidgoyal2014@gmail.com>
2021-05-24 21:44:23 +0530

    Update new_calc.js
    Revised the JavaScript file.
    Made it more readable
    Improvement - The input length is not fixed & will not go outside of the text area.

--- js/new_calc.js
+++ js/new_calc.js
@@ -4,6 +4,15 @@ var expr1 = "";
 var expr2 = "";
 var opr = "";
 
+function ifLimitExceed(){
+  if(expr1.length > 15 || expr2.length > 15){
+      return true;
+  }
+  else{
+      return false;
+  }
+}
+
 function oper(operate){
     if(opr == ""){
         opr = operate;
@@ -30,7 +39,6 @@ function erase(){
 }
 
 function append(num){
-
     // A number is entered
 
     if(opr == ""){
@@ -40,7 +48,12 @@ function append(num){
         expr2 = expr2 + num;
     }
 
-    display();
+    if(ifLimitExceed()){
+      reset_buffer();
+    }
+    else{
+        display();
+    }
 }
 
 function calculate(){
@@ -49,116 +62,22 @@ function calculate(){
     expr2 = "";
     opr = "";
 
-    display();
+    if(ifLimitExceed()){
+      reset_buffer();
+    }
+    else{
+        display();
+    }
 }
 
 function display(){
     res.innerHTML = expr1 + opr + expr2;
 }
 
-/*
-const calculator = {
-    displayValue: '0',
-    firstOperand: null,
-    waitingForSecondOperand: false,
-    operator: null,
-  };
-  
-  function inputDigit(digit) {
-    const { displayValue, waitingForSecondOperand } = calculator;
-  
-    if (waitingForSecondOperand === true) {
-      calculator.displayValue = digit;
-      calculator.waitingForSecondOperand = false;
-    } else {
-      calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-    }
-  }
-  
-  function inputDecimal(dot) {
-    // If the `displayValue` does not contain a decimal point
-    if (!calculator.displayValue.includes(dot)) {
-      // Append the decimal point
-      calculator.displayValue += dot;
-    }
-  }
-  
-  function handleOperator(nextOperator) {
-    const { firstOperand, displayValue, operator } = calculator
-    const inputValue = parseFloat(displayValue);
-  
-    if (operator && calculator.waitingForSecondOperand)  {
-      calculator.operator = nextOperator;
-      return;
-    }
-  
-    if (firstOperand == null) {
-      calculator.firstOperand = inputValue;
-    } else if (operator) {
-      const currentValue = firstOperand || 0;
-      const result = performCalculation[operator](currentValue, inputValue);
-  
-      calculator.displayValue = String(result);
-      calculator.firstOperand = result;
-    }
-  
-    calculator.waitingForSecondOperand = true;
-    calculator.operator = nextOperator;
-  }
-  
-  const performCalculation = {
-    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
-  
-    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
-  
-    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
-  
-    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
-  
-    '=': (firstOperand, secondOperand) => secondOperand
-  };
-  
-  function resetCalculator() {
-    calculator.displayValue = '0';
-    calculator.firstOperand = null;
-    calculator.waitingForSecondOperand = false;
-    calculator.operator = null;
-  }
-  
-  function updateDisplay() {
-    const display = document.querySelector('.calculator-screen');
-    display.value = calculator.displayValue;
-  }
-  
-  updateDisplay();
-  
-  const keys = document.querySelector('.calculator-keys');
-  keys.addEventListener('click', (event) => {
-    const { target } = event;
-    if (!target.matches('button')) {
-      return;
-    }
-  
-    if (target.classList.contains('operator')) {
-      handleOperator(target.value);
-          updateDisplay();
-      return;
-    }
-  
-    if (target.classList.contains('decimal')) {
-      inputDecimal(target.value);
-          updateDisplay();
-      return;
-    }
-  
-    if (target.classList.contains('all-clear')) {
-      resetCalculator();
-          updateDisplay();
-      return;
-    }
-  
-    inputDigit(target.value);
-    updateDisplay();
-  });
+function reset_buffer(){
+  expr1 = "";
+  expr2 = "";
+  opr = "";
 
-*/
+  res.innerHTML = "Limit Exceed";
+}
